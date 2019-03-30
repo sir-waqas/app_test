741988b833518b228a64312825f3d3e3d94bd8c1
Waqas Azam <waqas.mabsapps@gmail.com>
2019-03-31 04:36:52 +0500

    Changing the Welcome Message

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 /**
  * @format
  */
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+// import App from './App';
+import { name as appName } from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
+class App extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>AlSalaamuallykum</Text>
+            </View>
+        );
+    }
+}
 AppRegistry.registerComponent(appName, () => App);
