83f7e17a292d629b12d114924bbcc22b649a8b74
Waqas Azam <waqas.mabsapps@gmail.com>
2019-03-31 04:42:46 +0500

    Changing the Image URL

--- index.js
+++ index.js
@@ -2,15 +2,20 @@
  * @format
  */
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, Text, View, Image } from 'react-native';
 // import App from './App';
 import { name as appName } from './app.json';
 
 class App extends Component {
     render() {
+        let pic = {
+            // uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+            uri: 'http://mabsapps.com/wp-content/uploads/2018/11/cropped-mabsapps-logo-black-3.png'
+        };
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <Text>AlSalaamuallykum</Text>
+                <Image source={pic} style={{ width: 200, height: 100 }}></Image>
             </View>
         );
     }
